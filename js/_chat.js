fbd2cff41a0b2d593004701d5d61c77ebfeea770
John <jreynolds94@live.co.uk>
2023-03-09 01:54:29 +0000

    Update _chat.js

--- js/_chat.js
+++ js/_chat.js
@@ -40,11 +40,11 @@ window.onclick = function(event) {
 // Copy and edit data
 var _copy = new ClipboardJS('.copy_btn');
 _copy.on('success', function(e) {
-    layer.msg('复制成功',{time:600});
+    layer.msg('Copy Successful',{time:600});
     e.clearSelection();
 });
 _copy.on('error', function(e) {
-    layer.msg('复制成功',{time:1000});
+    layer.msg('Copy Successful',{time:1000}); //May have to be "Copy unSuccessful"
 });
 
 $(document).on('click','#edit_data', function(e) {
@@ -53,8 +53,8 @@ $(document).on('click','#edit_data', function(e) {
     layer.open({
         id: 1,
         type: 1,
-        title: '编辑问题',
-        btn: ['提交','取消'],
+        title: 'Edit Question',
+        btn: ['Submit','Cancel'], //May have to be lowercase
         content: `
         <div style='display:flex;justify-content:center;'>
             <textarea id='edit_box' oninput='auto_grow(this)'>${text}</textarea>
@@ -104,13 +104,13 @@ const msgerSendBtn = get(".send-btn");
 const restart_chat = get("#restart_chat");
 const BOT_IMG = "static/big_clever.svg";
 const PERSON_IMG = "static/human.svg";
-const BOT_NAME = "大聪明";
-const PERSON_NAME = "你";
+const BOT_NAME = "O-GPT";
+const PERSON_NAME = "Omni";
 // Function to delete chat history records for a user ID using the API
 function deleteChatHistory(userId) {
-    layer.confirm('这将清除历史聊天记录,你确定？', {
-        btn: ['确认','点错了'],
-        title: '启动新会话'
+    layer.confirm('This will clear the chat history, are you sure? ', {
+        btn: ['Confirm','Cancel'],  //Not sure about here
+        title: 'Start New Session'
     }, function(){
         layer.closeAll();
         layer.load(1, 0);
@@ -255,9 +255,9 @@ function appendMessage(name, img, side, text, date, id) {
     `;
     msgerChat.insertAdjacentHTML("beforeend", msgHTML);
     if (side == "left") {
-        $("."+holder).append(`<div title="点击复制" style="width:35px;margin:0 5 0 5;"><svg t="1676979873149" class="copy_btn" id="copy_data" data-clipboard-target=#a${id} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6847" width="25" height="25"><path d="M576 384a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64H234.666667a64 64 0 0 1-64-64V448a64 64 0 0 1 64-64h341.333333z m0 64H234.666667v341.333333h341.333333V448z m-64 192v64H298.666667v-64h213.333333zM789.333333 170.666667a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64h-106.666666v-64h106.666666V234.666667H448v106.666666h-64v-106.666666a64 64 0 0 1 64-64h341.333333zM512 533.333333v64H298.666667v-64h213.333333z" fill="#707070" p-id="6848"></path></svg></div>`);
+        $("."+holder).append(`<div title="Click To Copy" style="width:35px;margin:0 5 0 5;"><svg t="1676979873149" class="copy_btn" id="copy_data" data-clipboard-target=#a${id} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6847" width="25" height="25"><path d="M576 384a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64H234.666667a64 64 0 0 1-64-64V448a64 64 0 0 1 64-64h341.333333z m0 64H234.666667v341.333333h341.333333V448z m-64 192v64H298.666667v-64h213.333333zM789.333333 170.666667a64 64 0 0 1 64 64v341.333333a64 64 0 0 1-64 64h-106.666666v-64h106.666666V234.666667H448v106.666666h-64v-106.666666a64 64 0 0 1 64-64h341.333333zM512 533.333333v64H298.666667v-64h213.333333z" fill="#707070" p-id="6848"></path></svg></div>`);
     } else {
-        $("."+holder).append(`<div title="编辑提问" style="width:35px;margin:0 5 0 5;"><svg t="1677572686028" class=a${id} id="edit_data" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2880" width="20" height="20"><path d="M862.709333 116.042667a32 32 0 1 1 45.248 45.248L455.445333 613.813333a32 32 0 1 1-45.258666-45.258666L862.709333 116.053333zM853.333333 448a32 32 0 0 1 64 0v352c0 64.8-52.533333 117.333333-117.333333 117.333333H224c-64.8 0-117.333333-52.533333-117.333333-117.333333V224c0-64.8 52.533333-117.333333 117.333333-117.333333h341.333333a32 32 0 0 1 0 64H224a53.333333 53.333333 0 0 0-53.333333 53.333333v576a53.333333 53.333333 0 0 0 53.333333 53.333333h576a53.333333 53.333333 0 0 0 53.333333-53.333333V448z" fill="#707070" p-id="2881"></path></svg></div>`);
+        $("."+holder).append(`<div title="Edit Question" style="width:35px;margin:0 5 0 5;"><svg t="1677572686028" class=a${id} id="edit_data" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2880" width="20" height="20"><path d="M862.709333 116.042667a32 32 0 1 1 45.248 45.248L455.445333 613.813333a32 32 0 1 1-45.258666-45.258666L862.709333 116.053333zM853.333333 448a32 32 0 0 1 64 0v352c0 64.8-52.533333 117.333333-117.333333 117.333333H224c-64.8 0-117.333333-52.533333-117.333333-117.333333V224c0-64.8 52.533333-117.333333 117.333333-117.333333h341.333333a32 32 0 0 1 0 64H224a53.333333 53.333333 0 0 0-53.333333 53.333333v576a53.333333 53.333333 0 0 0 53.333333 53.333333h576a53.333333 53.333333 0 0 0 53.333333-53.333333V448z" fill="#707070" p-id="2881"></path></svg></div>`);
     }
     todown_now();
 }
@@ -294,7 +294,7 @@ function sendMsg(msg) {
         eventSource.onerror = function (e) {
             msgerSendBtn.disabled = false;
             console.log(e);
-            div.innerHTML += '已达到最大上下文限制，请开启新会话！';
+            div.innerHTML += 'The maximum context limit has been reached, please start a new session!';
             eventSource.close();
         };
     })
@@ -343,7 +343,7 @@ function uuidv4() {
 
 function randomString(len) {
     len = len || 32;
-    var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+    var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz';    /****The confusing characters Are Removed oOLl,9gq,Vv,Uu,I1****/
     var maxPos = $chars.length;
     var pwd = '';
     for (i = 0; i < len; i++) {
